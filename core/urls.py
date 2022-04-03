3dbd5d43d92384744ed995343f2e4f821bb017c5
Thomas Saied <tomas.temo77@gmail.com>
2022-04-03 18:55:56 +0200

    make html reusable

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about-us/', views.about, name='about'),
-    path('contact/', views.contact, name='about'),
+    path('contact/', views.contact, name='contact'),
 
 ]
