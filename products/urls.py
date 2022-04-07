91977db438c1fac9c7965f4278aafc6bfd2d0645
Thomas Saied <tomas.temo77@gmail.com>
2022-04-07 20:55:56 +0200

    add views

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 
-
-urlpatterns = []
+urlpatterns = [
+    path("categories/", views.all_categories, name="products"),
+    path("category-products/<int:pk>/", views.category_products, name="category_products"),
+    path("product/<int:pk>/", views.product_detail, name="product_detail"),
+    path("check/", views.check_product, name="check_code"),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.db.models import Q
+from .models import Category, Product, SKU
 
-# Create your views here.
+
+def all_categories(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    return render(
+        request,
+        "products.html",
+        context={"categories": categories, "products": products},
+    )
+
+
+def category_products(request, pk):
+    categories = Category.objects.filter(pk=pk)
+    products = Product.objects.filter(category_id=pk)
+    return render(
+        request,
+        "products.html",
+        context={"categories": categories, "products": products},
+    )
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+
+    related_products = Product.objects.filter(category=product.category).exclude(
+        pk=product.pk
+    )
+
+    return render(
+        request,
+        "product_detail.html",
+        context={"product": product, "related_products": related_products},
+    )
+
+
+def check_product(request):
+    qs = SKU.objects.all()
+    message = ""
+    serial = request.GET.get("serial", None)
+
+    if serial != "" and serial is not None:
+        new_qs = qs.filter(Q(serial_number__iexact=serial))
+        if new_qs:
+            message = "Your Product Is Correct"
+        else:
+            message = "wrong"
+
+    context = {
+        "queryset": qs,
+        "serial": serial,
+        "message": message,
+    }
+    return render(request , "check.html", context)
